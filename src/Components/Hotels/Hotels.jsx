df55c6100787e8d4d85b204ca018a94aab01c8db
pabitraghara <pabitraghara384@gmail.com>
2025-04-22 22:56:07 +0530

    change h to H

--- src/Components/Hotels/Hotels.jsx
+++ src/Components/Hotels/Hotels.jsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Hotels.css";
-import hotels from "./hotelsData";
+import hotels from "./HotelsData";
 
 
 const Hotels = () => {
